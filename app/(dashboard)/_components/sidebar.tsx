24faddd4591a4b047b968e4602c3b13b4eb44e94
Nexus Simple <nexus.simple7@gmail.com>
2024-01-19 00:48:27 +0530

    [FEATURE] Add basic styling to the sidebar component + Logo

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -1,5 +1,15 @@
+import { Logo } from "@/app/(dashboard)/_components/logo";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    // 👇 Dark background
+    // <div className="h-full border-r bg-[#100133] text-white shadow-[0px_20px_20px_10px_#00000024]">
+    <div className="h-full border-r shadow-[0px_0px_17px_5px_#00000024]">
+      <div className="p-6">
+        <Logo />
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
