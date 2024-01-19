eed0c201726744f0545e02ae28cd0cb61dd70e7e
Nexus Simple <nexus.simple7@gmail.com>
2024-01-19 23:53:21 +0530

    [FEATURE] Increase the background opacity of the active Sidebar item.

--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -32,7 +32,7 @@ export const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
       className={cn(
         "ml-4 flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
         isActive &&
-          "rounded-tl-lg rounded-bl-lg text-[#190359] bg-[#94157b] bg-opacity-15 hover:bg-[#94157b] hover:bg-opacity-15 hover:text-[#190359]"
+          "rounded-tl-lg rounded-bl-lg text-[#190359] bg-[#94157b] bg-opacity-40 hover:bg-[#94157b] hover:bg-opacity-15 hover:text-[#190359]"
         //   "text-[#190359]  hover:text-[#190359]"
       )}
     >
