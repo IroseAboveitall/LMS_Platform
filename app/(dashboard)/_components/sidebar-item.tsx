795c5455c7d7880055972f23ba643c07d1e65e63
Nexus Simple <nexus.simple7@gmail.com>
2024-01-20 01:00:11 +0530

    [FEATURE] Create a curved outside border radius for the active item
    Created a curved outside border radius for the active Sidebar item.
    But need to change the background color for a good looking UI Sidebar.

--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -1,6 +1,8 @@
 "use client";
 // 👆 For using hooks like usePathname()
 
+import "./sidebar.css";
+
 import { cn } from "@/lib/utils";
 import { LucideIcon } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
@@ -32,10 +34,11 @@ export const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
       className={cn(
         "ml-4 flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
         isActive &&
-          "rounded-tl-lg rounded-bl-lg text-[#190359] bg-[#94157b] bg-opacity-40 hover:bg-[#94157b] hover:bg-opacity-15 hover:text-[#190359]"
-        //   "text-[#190359]  hover:text-[#190359]"
+          "active rounded-tl-lg rounded-bl-lg text-[#190359] bg-[#94157b] bg-opacity-40 hover:bg-[#94157b] hover:bg-opacity-40 hover:text-[#190359]"
       )}
     >
+      <b></b>
+      <b></b>
       <div className="flex items-center gap-x-2 py-4">
         <Icon
           size={22}
