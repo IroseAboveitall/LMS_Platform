305890c5f8fcf7cd87f85fed8b268d03a9ba52b3
Nexus Simple <nexus.simple7@gmail.com>
2024-01-20 21:17:24 +0530

    [FEATURE] Convert Plain CSS to Tailwind Equivalent [BUG]
    - Converted the Plain CSS for the Curved Outside Border Radius to its Tailwind equivalent.
    - About the Bug : the dots appear only when we quickly switch tabs in the Sidebar.
        -- Solution for future : Change the background color of the Sidebar to a Lighter color.

--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -1,8 +1,6 @@
 "use client";
 // 👆 For using hooks like usePathname()
 
-import "./sidebar.css";
-
 import { cn } from "@/lib/utils";
 import { LucideIcon } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
@@ -32,13 +30,23 @@ export const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
       onClick={onClick}
       type="button"
       className={cn(
-        "ml-4 rounded-tl-[20px] rounded-bl-[20px] flex items-center gap-x-2 text-[#d3efff] text-sm font-[500] pl-6 transition-all duration-0 hover:text-[#67eaf7]",
+        "relative ml-2 rounded-tl-[20px] rounded-bl-[20px] flex items-center gap-x-2 text-[#d3efff] text-sm font-[500] pl-6 transition-all duration-0 hover:text-[#67eaf7]",
         isActive &&
-          "active  text-[#0c0f2e] bg-white hover:bg-white hover:text-[#0c0f2e]"
+          "text-[#0c0f2e] bg-white hover:bg-white hover:text-[#0c0f2e]"
       )}
     >
-      <b className="z-10 pointer-events-none"></b>
-      <b className="z-10 pointer-events-none"></b>
+      <b
+        className={cn(
+          "z-10 pointer-events-none absolute top-[-20px] h-[20px] w-full right-0 bg-white hidden before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-[#0c0f2e] before:rounded-br-[20px]",
+          isActive && "block"
+        )}
+      ></b>
+      <b
+        className={cn(
+          "z-10 pointer-events-none absolute bottom-[-20px] right-0 h-[20px] w-full bg-white hidden before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-[#0c0f2e] before:rounded-tr-[20px]",
+          isActive && "block"
+        )}
+      ></b>
       <div className="z-40 pointer-events-none flex items-center gap-x-2 py-4">
         <Icon size={22} className={cn(isActive && "scale-125")} />
         {label}
