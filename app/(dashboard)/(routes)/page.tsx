31967e8922cf04b6104f778cedf7d19b8cf52907
Nexus Simple <nexus.simple7@gmail.com>
2024-01-20 21:59:30 +0530

    [FEATURE] Add a Navbar that sticks at the top , right of the Sidebar.
    Added the Navbar by wrapping it with a div in the layout file in the (dashboard) route group.

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <div></div>;
-}
+// export default function Home() {
+//   return <div>Home Page or Dashboard</div>;
+// }
+// 👆 Above was present from the starter code of NextJS
+
+const Home = () => {
+  return <div>Home Page or Dashboard</div>;
+};
+
+export default Home;
