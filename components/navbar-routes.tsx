456e29516d933b7004e2536209020ad5fba60d3b
Nexus Simple <nexus.simple7@gmail.com>
2024-01-21 14:47:57 +0530

    [FEATURE] Add the Navbar Routes Component to the Navbar.
    Added the Navbar Routes Component to the Navbar , currently only the User button inside the
    Navbar Routes Component.
    This User button is simply a shadcn Button Component.
        -- But Later add Authentication to it, after implementing Authentication to
           this project.

---
+++ components/navbar-routes.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+export const NavbarRoutes = () => {
+  return (
+    <div className="flex gap-x-2 ml-auto">
+      {/* TODO */}
+      <Button size="sm" variant="userButton" className="rounded-full">
+        User
+      </Button>
+    </div>
+  );
+};
