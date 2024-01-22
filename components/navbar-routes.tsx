86c9c10a37057cb410581eb32c0e8297f95cab99
Nexus Simple <nexus.simple7@gmail.com>
2024-01-22 14:38:32 +0530

    [BUG] Put the Link outside of the Button again for Click malfunction.
    --Put the Link tag outside of the Button like it was previously before doing the last commit.
        - Done because the Button component of shadcn must be having some margin or padding,
          So when I clicked on the edge, the Click functionality to redirect to the HREF pointed by
          the Link tag was not responding.
    
    -- Also removed the margin right of the Button to put it inside the Link tag.
        - Done because extra margin appeared when using Focus Navigation using the Tab.

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -14,24 +14,26 @@ export const NavbarRoutes = () => {
   return (
     <div className="flex gap-x-2 ml-auto">
       {isInstructorPage || isLearnerPage ? (
-        <Button
-          size="sm"
-          className="mr-5 bg-[#11555b] hover:text-[#67eaf7] hover:bg-[#11555b]"
-        >
-          <Link href="/" className="flex">
+        <Link href="/" className="mr-4">
+          <Button
+            size="sm"
+            className=" bg-[#11555b] hover:text-[#67eaf7] hover:bg-[#11555b]"
+          >
             <LogOut className="h-4 w-4 mr-2" />
             Leave
-          </Link>
-        </Button>
+          </Button>
+        </Link>
       ) : (
         // <Button className="mr-5 bg-[#11555b] hover:bg-[#11555be7]">
 
-        <Button
-          size="sm"
-          className="mr-5 bg-[#11555b] hover:text-[#67eaf7] hover:bg-[#11555b]"
-        >
-          <Link href="/instructor/courses">Instructor Dashboard</Link>
-        </Button>
+        <Link href="/instructor/courses" className="mr-4">
+          <Button
+            size="sm"
+            className=" bg-[#11555b] hover:text-[#67eaf7] hover:bg-[#11555b]"
+          >
+            Instructor Dashboard
+          </Button>
+        </Link>
       )}
       {/* TODO : Implement Authentication Button*/}
       <Button size="sm" variant="userButton" className="rounded-full">
