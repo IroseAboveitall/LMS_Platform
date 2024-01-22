4d49dce520c54157a0524dfca40ab42f7701ca65
Nexus Simple <nexus.simple7@gmail.com>
2024-01-22 13:50:41 +0530

    [FEATURE] Add a Link to the Leave Button

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -3,11 +3,10 @@
 import { Button } from "@/components/ui/button";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 export const NavbarRoutes = () => {
   const pathname = usePathname();
-  const router = useRouter();
 
   const isInstructorPage = pathname?.startsWith("/instructor");
   const isLearnerPage = pathname?.includes("/chapter");
@@ -15,10 +14,12 @@ export const NavbarRoutes = () => {
   return (
     <div className="flex gap-x-2 ml-auto">
       {isInstructorPage || isLearnerPage ? (
-        <Button className="mr-5 bg-[#11555b] hover:text-[#67eaf7] hover:bg-[#11555b]">
-          <LogOut className="h-4 w-4 mr-2" />
-          Leave
-        </Button>
+        <Link href="/">
+          <Button className="mr-5 bg-[#11555b] hover:text-[#67eaf7] hover:bg-[#11555b]">
+            <LogOut className="h-4 w-4 mr-2" />
+            Leave
+          </Button>
+        </Link>
       ) : (
         // <Button className="mr-5 bg-[#11555b] hover:bg-[#11555be7]">
         <Link href="/instructor/courses">
