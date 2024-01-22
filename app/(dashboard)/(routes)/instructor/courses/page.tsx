72a47d57190dc3fdbc8cace9523168f9877e3428
Nexus Simple <nexus.simple7@gmail.com>
2024-01-22 15:47:24 +0530

    [FEATURE] Add a Link & a Button for creating a course in the CoursesPage

--- app/(dashboard)/(routes)/instructor/courses/page.tsx
+++ app/(dashboard)/(routes)/instructor/courses/page.tsx
@@ -1,5 +1,16 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const CoursesPage = () => {
-  return <div>Courses Page</div>;
+  return (
+    <div className="p-6">
+      <Link href="/instructor/create">
+        <Button className="bg-[#182B48] hover:bg-[#234661] transition">
+          Create Course
+        </Button>
+      </Link>
+    </div>
+  );
 };
 
 export default CoursesPage;
