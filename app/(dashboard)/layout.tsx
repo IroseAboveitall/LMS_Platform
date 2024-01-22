b526787f9e834d653d0e7659fbc9e797fe47222b
Nexus Simple <nexus.simple7@gmail.com>
2024-01-22 14:48:58 +0530

    [BUG] Add a padding top to the Main content in the Dashboard Layout.
    - The main content for the pages was not showing because the Navbar had a height.
      So pushed the main content down by the same amount of height as the Navbar.

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,9 @@
 import Sidebar from "@/app/(dashboard)/_components/sidebar";
 import { Navbar } from "@/app/(dashboard)/_components/navbar";
 
-
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
-
       <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
         <Navbar />
       </div>
@@ -15,7 +13,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       </div>
 
       {/* Push the content other than the sidebar to the right by the same amount of padding as is the width of the Sidebar */}
-      <main className="md:pl-56">{children}</main>
+      <main className="md:pl-56 pt-[80px]">{children}</main>
     </div>
   );
 };
