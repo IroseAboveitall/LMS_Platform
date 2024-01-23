f55e02da9dce6088e6ae3795fd19efaf5f4b78e6
Nexus Simple <nexus.simple7@gmail.com>
2024-01-23 13:56:12 +0530

    [FEATURE] Add a basic form Schema + Install axios + Imports
    - Added a basic form Schema with only title at the moment.
    - Installed axios package
    - Imported all packages necessary to create the CreateCourse form.

--- app/(dashboard)/(routes)/instructor/create/page.tsx
+++ app/(dashboard)/(routes)/instructor/create/page.tsx
@@ -1,3 +1,32 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+// import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+// We could create this Schema in a file inside a folder schemas, so that it could be accessible from everywhere.
+const formSchema = z.object({
+  title: z.string().min(1, {
+    message: "Title is required",
+  }),
+});
+
 const CreateCoursePage = () => {
   return <div>Create a Course</div>;
 };
