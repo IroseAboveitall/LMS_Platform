9d8d12b503970b75290397509c67f9dcde3de398
Nexus Simple <nexus.simple7@gmail.com>
2024-01-24 19:06:06 +0530

    [FEATURE] Customized the error Toast notificaion.
    Customized the error Toast notificaion by
        - increasing the duration from default of 4000 for error to 7000.
        - changed the background & text color of the notification.
        - changed the primary and secondary color of the icon.

--- components/providers/toaster-provider.tsx
+++ components/providers/toaster-provider.tsx
@@ -1,7 +1,23 @@
-"use client"
+"use client";
 
-import { Toaster } from "react-hot-toast"
+import { Toaster } from "react-hot-toast";
 
 export const ToastProvider = () => {
-    return <Toaster />
-}
+  return (
+    <Toaster
+      toastOptions={{
+        error: {
+          duration: 7000,
+          style: {
+            background: "#ff3640",
+            color: "#fff",
+          },
+          iconTheme: {
+            primary: "white",
+            secondary: "red",
+          },
+        },
+      }}
+    />
+  );
+};
