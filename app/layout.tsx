93e1e66c42aa33288ea4a9ed9ee34d99eebe6428
Nexus Simple <nexus.simple7@gmail.com>
2024-01-25 19:05:57 +0530

    [FEATURE] Setup Clerk Authentication from Docs after installing.
    - Need to create the sign-in and sign-out pages next

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ToastProvider } from '@/components/providers/toaster-provider'
+import { ClerkProvider } from '@clerk/nextjs'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,11 +18,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ToastProvider/>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ToastProvider/>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
