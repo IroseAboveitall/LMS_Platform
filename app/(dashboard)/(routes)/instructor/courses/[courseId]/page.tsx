916ce7b1327576a2691074e59fb8e10440d892b8
Nexus Simple <nexus.simple7@gmail.com>
2024-01-26 23:58:21 +0530

    [FEATURE] Put the variants icon component in the Course Edit page.

--- app/(dashboard)/(routes)/instructor/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/instructor/courses/[courseId]/page.tsx
@@ -1,5 +1,7 @@
+import { IconComp } from "@/components/icon-comp";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
+import { Pencil } from "lucide-react";
 import { redirect } from "next/navigation";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
@@ -39,10 +41,8 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
     <div className="p-6">
       {/* 👇 Title */}
       <div className="flex items-center justify-between">
-
         {/* 👈 This is the container to center its only child which is the container div below*/}
         <div className="flex flex-col gap-y-2">
-
           {/* 👈 This is the container for h1 & span*/}
           <h1 className="text-2xl font-medium">Course Setup</h1>
           <span className="text-sm text-slate-700">
@@ -55,6 +55,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
         <div>
           <div className="flex items-center gap-x-2">
+            <IconComp icon={Pencil} />
             <h2 className="text-xl">Customize your course</h2>
           </div>
         </div>
