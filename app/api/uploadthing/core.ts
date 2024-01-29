539bbe30f22d46a8eea951fb0bca65e3b8d3ab4e
Nexus Simple <nexus.simple7@gmail.com>
2024-01-29 23:28:37 +0530

    [FEATURE] Create the basic FileRoute for uploadthing as described in the DOCS
    -- Removed the body of the ourFileRouter
        - To be created as per our needs next.

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,10 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
+
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
