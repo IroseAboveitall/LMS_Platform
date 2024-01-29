08826541e5f3b5aa20bd2f0f0a8f0bb76837d893
Nexus Simple <nexus.simple7@gmail.com>
2024-01-29 21:15:20 +0530

    [FEATURE] Create the PATCH route handler to Update the Course Title

---
+++ app/api/courses/[courseId]/route.ts
@@ -0,0 +1,30 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { courseId: string } } // This should be the 2nd parameter
+) {
+  try {
+    const { userId } = auth();
+    const { courseId } = params;
+
+    const data = await req.json();
+
+    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+
+    const course = await db.course.update({
+      where: { id: courseId, userId },
+      data: {
+        ...data,
+      },
+    });
+
+    // Send the updated course to the Client Side
+    return NextResponse.json(course);
+  } catch (error) {
+    console.log("[COURSE_ID]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
