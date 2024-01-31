ca5821054a0417cda2c19dfe13288d5684502ada
Nexus Simple <nexus.simple7@gmail.com>
2024-01-31 16:48:05 +0530

    [FEATURE+BUG] Add some config for UploadThing to work while in development.
    - Also added some config for images to work when uploading with UploadThing.
        - utfs.io as the hostname ( UploadThing hostname )

--- app/(dashboard)/(routes)/instructor/courses/[courseId]/_components/image-form.tsx
+++ app/(dashboard)/(routes)/instructor/courses/[courseId]/_components/image-form.tsx
@@ -6,19 +6,11 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { ImageIcon, Pencil, PlusCircle } from "lucide-react";
 
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from "@/components/ui/form";
+
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
-import { cn } from "@/lib/utils";
-import { Textarea } from "@/components/ui/textarea";
 import { Course } from "@prisma/client";
 import Image from "next/image";
 import { FileUpload } from "@/components/file-upload";
