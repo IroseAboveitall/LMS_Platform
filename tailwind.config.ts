ed1e53643db61aff11d99d11e4d30175c0c7874a
Nexus Simple <nexus.simple7@gmail.com>
2024-01-31 13:53:32 +0530

    [FEATURE] Wrapped the whole config object in tailwind.config.ts file with withUt from UploadThing.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,17 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
-const config = {
+// 👇 Import this for adding the styles for UploadThing
+import { withUt } from "uploadthing/tw";
+
+// 👇 Wrap the whole config object with "withUt" which is imported above
+const config = withUt({
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -75,6 +79,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+}) satisfies Config;
 
-export default config
+export default config;
