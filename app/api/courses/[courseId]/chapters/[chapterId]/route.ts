8f8c49adaf104f836af8f534f0be9417f4a58850
Nexus Simple <nexus.simple7@gmail.com>
2024-02-07 18:38:22 +0530

    [FEATURE] Create the route handler for the PATCH request for editing the chapter title.

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -0,0 +1,44 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized access", { status: 401 });
+    }
+
+    // Check if the user who is trying to reorder the chapters is the owner of the course
+    const courseOwner = await db.course.findUnique({
+      where: { id: params.courseId, userId },
+    });
+
+    if (!courseOwner) {
+      return new NextResponse("Unauthorized access", { status: 401 });
+    }
+
+    const { isPublished, ...updatedValues } = await req.json();
+
+    const chapter = await db.chapter.update({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+      data: {
+        ...updatedValues,
+      },
+    });
+
+    // TODO : Handle Video Upload
+
+    return NextResponse.json(chapter);
+  } catch (error) {
+    console.log("[COURSES_CHAPTER_ID]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
