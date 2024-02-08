e58a79bbdb94097ea8fb8e0f456e917254e589b4
Nexus Simple <nexus.simple7@gmail.com>
2024-02-08 11:38:52 +0530

    [FEATURE] Create a basic component for Text Editing using react-quill.
    - Used useMemo hook with empty dependency array to render only on the 1st initial render.
    - Used dynamic() of next to dynamically import (asynchronously load) the ReactQuill from the server side.
    - Used { ssr : false } to ensure that the component is only loaded and rendered on the client side.

---
+++ components/editor.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+import "react-quill/dist/quill.snow.css";
+
+interface EditorProps {
+  onChange: (value: string) => void;
+  value: string; // Current value
+}
+
+export const Editor = ({ onChange, value }: EditorProps) => {
+  const ReactQuill = useMemo(
+    () => dynamic(() => import("react-quill"), { ssr: false }),
+    []
+  );
+  // https://chat.openai.com/share/d2f7c222-0c6d-4fda-9878-065383b4227d
+};
