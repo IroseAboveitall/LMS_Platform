1cf5baab6d094d74d0ba870a4793a0529f6fe920
Nexus Simple <nexus.simple7@gmail.com>
2024-02-09 16:03:22 +0530

    [FEATURE] Handle Video Upload in the route handler for Chapter Updating.

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,7 +1,12 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
+import Mux from "@mux/mux-node";
 
+const mux = new Mux({
+  tokenId: process.env.MUX_TOKEN_ID!,
+  tokenSecret: process.env.MUX_TOKEN_SECRET!,
+});
 export async function PATCH(
   req: Request,
   { params }: { params: { courseId: string; chapterId: string } }
@@ -34,8 +39,40 @@ export async function PATCH(
       },
     });
 
-    // TODO : Handle Video Upload
+    // Handle Video Upload
+    if (updatedValues.videoUrl) {
+      // Find existing Mux data : If user changes the video, we need to delete the asset from Mux and then create a new one
 
+      // 👇 Delete existing Mux data if it exists
+      const existingMuxData = await db.muxData.findFirst({
+        where: {
+          chapterId: params.chapterId,
+        },
+      });
+
+      if (existingMuxData) {
+        await mux.video.assets.delete(existingMuxData.assetId);
+        await db.muxData.delete({
+          where: {
+            id: existingMuxData.id,
+          },
+        });
+      }
+
+      const asset = await mux.video.assets.create({
+        input: updatedValues.videoUrl,
+        playback_policy: ["public"],
+        test: false,
+      });
+
+      await db.muxData.create({
+        data: {
+          assetId: asset.id,
+          chapterId: params.chapterId,
+          playbackId: asset.playback_ids?.[0]?.id,
+        },
+      });
+    }
     return NextResponse.json(chapter);
   } catch (error) {
     console.log("[COURSES_CHAPTER_ID]", error);
