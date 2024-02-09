3462dad16d51d5357b2a1eb2a4d8ecf713aae3b0
Nexus Simple <nexus.simple7@gmail.com>
2024-02-09 11:20:14 +0530

    [FEATURE] Create ChapterVideo Component by copying the ImageForm Component from Courses components.

---
+++ app/(dashboard)/(routes)/instructor/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { ImageIcon, Pencil, PlusCircle } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { Course } from "@prisma/client";
+import Image from "next/image";
+import { FileUpload } from "@/components/file-upload";
+
+interface ImageFormProps {
+  initialData: Course;
+  courseId: string;
+}
+
+const formSchema = z.object({
+  imageUrl: z.string().min(1, {
+    message: "Image cannot be empty",
+  }),
+});
+
+export const ImageForm = ({ initialData, courseId }: ImageFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleEdit = () => setIsEditing((current) => !current);
+
+  const router = useRouter();
+
+  // Create the onSubmit function handler to call the API to submit the inputs.
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(`/api/courses/${courseId}`, data);
+      toast.success("Course updated successfully");
+      toggleEdit();
+      router.refresh();
+    } catch {
+      toast.error("Well, this is awkward. Something went haywire.");
+    }
+  };
+
+  return (
+    <div className="mt-6 border bg-[#67ebf71f] rounded-md p-4">
+      {/* 👇 Container for the "Course Image" & the Button */}
+      <div className="font-medium flex items-center justify-between">
+        Course image
+        <Button
+          onClick={toggleEdit}
+          variant="ghost"
+          className="hover:bg-slate-300 flex justify-center mr-2"
+        >
+          {isEditing && <>Cancel</>}
+          {!isEditing && !initialData.imageUrl && (
+            <>
+              <PlusCircle className="h-4 w-4 mr-2" />
+              Add an image
+            </>
+          )}
+          {!isEditing && initialData.imageUrl && (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              Change image
+            </>
+          )}
+        </Button>
+      </div>
+
+      {/* 👇 Display the current Course Image if user has not clicked the "Change Image" Button */}
+      {!isEditing &&
+        (!initialData.imageUrl ? (
+          <div className="mt-4 flex items-center justify-center h-60 bg-slate-200 rounded-md">
+            <ImageIcon className="h-10 w-10 text-slate-500" />
+          </div>
+        ) : (
+          <div className="relative aspect-video mt-2">
+            <Image
+              alt="Upload"
+              fill
+              className="object-cover rounded-md"
+              src={initialData.imageUrl}
+            />
+          </div>
+        ))}
+
+      {/* 👇 Display the form if user has clicked the "Edit Description" Button */}
+      {isEditing && (
+        <div>
+          <FileUpload
+            endpoint="courseImage"
+            onChange={(url) => {
+              if (url) {
+                onSubmit({ imageUrl: url });
+              }
+            }}
+          />
+          <div className="text-xs text-muted-foreground mt-4">
+            16:9 aspect ratio recommended
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
