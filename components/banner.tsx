b7dd9fd05af3b2cddbd60a702b1802681628f1f6
Nexus Simple <nexus.simple7@gmail.com>
2024-02-10 12:15:34 +0530

    [FEATURE] Create the banner variants : warning or success variants.

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,3 +1,20 @@
+import { cva } from "class-variance-authority";
+
+const bannerVariants = cva(
+  "border text-center p-4 text-sm flex items-center w-full",
+  {
+    variants: {
+      variant: {
+        warning: "bg-yellow-200/80 border-yellow-30 text-primary",
+        success: "bg-emerald-700 border-emerald-800 text-secondary",
+      },
+    },
+    defaultVariants: {
+      variant: "warning",
+    },
+  }
+);
+
 export const Banner = () => {
   return <div>Banner</div>;
 };
