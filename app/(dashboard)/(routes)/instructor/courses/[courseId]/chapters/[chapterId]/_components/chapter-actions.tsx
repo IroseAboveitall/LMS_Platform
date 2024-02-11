96f19cbbce8e0a154257a096b9b5d4f5f912e7f5
Nexus Simple <nexus.simple7@gmail.com>
2024-02-11 13:26:48 +0530

    [FEATURE] Create the function handler for the deleting a chapter in the ChapterActions component.
    - Also created a state "isLoading" to toggle between true and false during the try and finally block.

--- app/(dashboard)/(routes)/instructor/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
+++ app/(dashboard)/(routes)/instructor/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -2,7 +2,11 @@
 
 import { ConfirmModal } from "@/components/modals/confirm-modal";
 import { Button } from "@/components/ui/button";
+import axios from "axios";
 import { Trash, TrashIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface ChapterActionsProps {
   disabled: boolean;
@@ -17,6 +21,22 @@ export const ChapterActions = ({
   chapterId,
   isPublished,
 }: ChapterActionsProps) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+      await axios.delete(`/api/courses/${courseId}/chapter/${chapterId}`);
+      toast.success("Chapter deleted successfully");
+      router.refresh();
+      router.push(`/instructor/courses/${courseId}`);
+    } catch {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <div className="flex items-center gap-x-2">
       <Button
