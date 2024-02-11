15c647aac697c1786b174ace2c93121f340b62ca
Nexus Simple <nexus.simple7@gmail.com>
2024-02-11 13:17:33 +0530

    [FEATURE] Create the Template using the different Elements of the alert-dialog component.

--- components/modals/confirm-modal.tsx
+++ components/modals/confirm-modal.tsx
@@ -17,4 +17,22 @@ interface ConfirmModalProps {
   onConfirm: () => void;
 }
 
-export const ConfirmModal = ({ children, onConfirm }: ConfirmModalProps) => {};
+export const ConfirmModal = ({ children, onConfirm }: ConfirmModalProps) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={onConfirm}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
