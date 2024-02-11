8a5466d248e1f35b3a78b8d4cdbebf8d28f49f78
Nexus Simple <nexus.simple7@gmail.com>
2024-02-11 14:13:07 +0530

    [FEATURE] Create the API for deleting a chapter.

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -7,6 +7,86 @@ const mux = new Mux({
   tokenId: process.env.MUX_TOKEN_ID!,
   tokenSecret: process.env.MUX_TOKEN_SECRET!,
 });
+
+// API to delete a Chapter
+export async function DELETE(
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized access", { status: 401 });
+    }
+
+    // Check if the user who is trying to delete the chapter is the owner of the course
+    const courseOwner = await db.course.findUnique({
+      where: { id: params.courseId, userId },
+    });
+
+    if (!courseOwner) {
+      return new NextResponse("Unauthorized access", { status: 401 });
+    }
+
+    // Find the chapter
+    const chapter = await db.chapter.findUnique({
+      where: { id: params.chapterId, courseId: params.courseId },
+    });
+
+    if (!chapter) {
+      return new NextResponse("Chapter not found", { status: 404 });
+    }
+
+    // Check if the chapter that the user is trying to delete has a videoUrl. If present, delete the muxData from both mux and DB
+    if (chapter.videoUrl) {
+      const existingMuxData = await db.muxData.findFirst({
+        where: {
+          chapterId: params.chapterId,
+        },
+      });
+
+      if (existingMuxData) {
+        await mux.video.assets.delete(existingMuxData.assetId);
+        await db.muxData.delete({
+          where: {
+            id: existingMuxData.id,
+          },
+        });
+      }
+    }
+
+    const deletedChapter = await db.chapter.delete({
+      where: { id: params.chapterId, courseId: params.courseId },
+    });
+
+    // After the chapter is deleted, is there still a chapter in this course that is published? If there isn't, i.e. this chapter which was deleted was published, that means that we are going to have to unpublish the entire course
+
+    const publishedChaptersInCourse = await db.chapter.findMany({
+      where: {
+        courseId: params.courseId,
+        isPublished: true,
+      },
+    });
+
+    //  If the publishedChaptersInCourse is empty
+    if (!publishedChaptersInCourse.length) {
+      await db.course.update({
+        where: { id: params.courseId },
+        data: {
+          isPublished: false,
+        },
+      });
+    }
+
+    return NextResponse.json(deletedChapter);
+  } catch (error) {
+    console.log("[CHAPTER_ID_DELETE]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+// API to Update a Chapter
 export async function PATCH(
   req: Request,
   { params }: { params: { courseId: string; chapterId: string } }
